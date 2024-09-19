0318e0c9b562b2e16e8a067070705c353fb0f491
Giulio de Cadilhac <g.decadilhac@gmail.com>
2024-09-19 16:21:34 +0100

    feat: Implement bot logic up to findings.push

--- Challenge2/src/agent.ts
+++ Challenge2/src/agent.ts
@@ -1,51 +1,66 @@
-import { Finding, FindingSeverity, FindingType, HandleTransaction, TransactionEvent, ethers } from "forta-agent";
-import { CREATE_AGENT_ABI, UPDATE_AGENT_ABI, NETHERMIND_ADDRESS, FORTA_REGISTRY_ADDRESS } from "./constants";
-
-// getPoolValues
-// computePoolAddress
-// isUniswapPoolAddress
+import {
+  Finding,
+  FindingSeverity,
+  FindingType,
+  HandleTransaction,
+  TransactionEvent,
+  getEthersProvider,
+  ethers,
+} from "forta-agent";
+import { Provider } from "@ethersproject/providers";
+import { UNI_SWAP_EVENT_ABI, UNI_POOL_FUNCTIONS_ABI, UNI_FACTORY_ADDRESS, UNI_INIT_CODE_HASH } from "./constants";
 
 export function provideHandleTransaction(
-  UNI_FACTORY_ADDRESS: string,
-  UNI_SWAP_EVENT_ABI: string,
-  provider: ethers.providers.JsonRpcProvider
+  uniFactoryAddress: string,
+  uniInitCode: string,
+  uniSwapEventAbi: string,
+  provider: Provider
 ): HandleTransaction {
   return async (txEvent: TransactionEvent) => {
     const findings: Finding[] = [];
 
-    if (txEvent.from.toLowerCase() !== nethermindAddress.toLowerCase()) return findings;
-    if (txEvent.to?.toLowerCase() !== fortaRegistryAddress.toLowerCase()) return findings;
+    // todo: if none of txEvent.addresses match the pool addresses in the LRU cache, return
 
-    const filteredTxEvents = txEvent.filterFunction([createAgentAbi, updateAgentAbi], fortaRegistryAddress);
+    const filteredLogs = txEvent.filterLog(uniSwapEventAbi);
 
-    filteredTxEvents.forEach((event) => {
-      const isDeployment = event.name === "createAgent";
-      findings.push(
-        Finding.fromObject({
-          name: isDeployment ? "Nethermind Bot Deployment" : "Nethermind Bot Update",
-          description: `Nethermind ${
-            isDeployment ? "deployed a new" : "updated an existing"
-          } bot with ID: ${event.args.agentId?.toString()}`,
-          alertId: isDeployment ? "NEW-BOT-DEPLOYED" : "EXISTING-BOT-UPDATED",
-          severity: FindingSeverity.Low,
-          type: FindingType.Info,
-          metadata: {
-            agentId: event.args.agentId?.toString(),
-            chainId: "137",
-          },
-        })
+    for (const filteredLog of filteredLogs) {
+      const interceptedPoolAddress = filteredLog.address;
+      const interceptedPoolContract = new ethers.Contract(interceptedPoolAddress, uniSwapEventAbi, provider);
+      const interceptedPoolValues: any[] = await Promise.all([
+        interceptedPoolContract.token0({ blockTag: txEvent.blockNumber }),
+        interceptedPoolContract.token1({ blockTag: txEvent.blockNumber }),
+        interceptedPoolContract.fee({ blockTag: txEvent.blockNumber }),
+      ]);
+      const interceptedPoolValuesBytes = ethers.utils.defaultAbiCoder.encode(
+        ["address", "address", "uint24"],
+        interceptedPoolValues
       );
-    });
+      const interceptedSalt = ethers.utils.solidityKeccak256(["bytes"], [interceptedPoolValuesBytes]);
+      const realPoolAddress = ethers.utils.getCreate2Address(uniFactoryAddress, interceptedSalt, uniInitCode);
+      const isUniPool = realPoolAddress.toLowerCase() === interceptedPoolAddress.toLowerCase();
+      if (!isUniPool) return findings;
+
+      // findings.push(
+      //   Finding.fromObject({
+      //     name: isDeployment ? "Nethermind Bot Deployment" : "Nethermind Bot Update",
+      //     description: `Nethermind ${
+      //       isDeployment ? "deployed a new" : "updated an existing"
+      //     } bot with ID: ${event.args.agentId?.toString()}`,
+      //     alertId: isDeployment ? "NEW-BOT-DEPLOYED" : "EXISTING-BOT-UPDATED",
+      //     severity: FindingSeverity.Low,
+      //     type: FindingType.Info,
+      //     metadata: {
+      //       agentId: event.args.agentId?.toString(),
+      //       chainId: "137",
+      //     },
+      //   })
+      // );
+    }
 
     return findings;
   };
 }
 
 export default {
-  handleTransaction: provideHandleTransaction(
-    CREATE_AGENT_ABI,
-    UPDATE_AGENT_ABI,
-    NETHERMIND_ADDRESS,
-    FORTA_REGISTRY_ADDRESS
-  ),
+  handleTransaction: provideHandleTransaction(UNI_FACTORY_ADDRESS, UNI_SWAP_EVENT_ABI, getEthersProvider()),
 };
--- Challenge2/src/constants.ts
+++ Challenge2/src/constants.ts
@@ -4,8 +4,8 @@ export const UPDATE_AGENT_ABI = "function updateAgent(uint256 agentId,string met
 export const NETHERMIND_ADDRESS = "0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8";
 export const FORTA_REGISTRY_ADDRESS = "0x61447385B019187daa48e91c55c02AF1F1f3F863";
 
-export const MOCK_CHAIN_IDS = [137];
-export const MOCK_AGENT_ID = 1;
+export const CHAIN_IDS = [137];
+// export const MOCK_AGENT_ID = 1;
 
 export const UNI_SWAP_EVENT_ABI =
   "event Swap (address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)";
