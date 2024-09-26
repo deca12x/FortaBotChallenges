089ffa1253f4ea23b13cc90be3a77109dfdd0746
Giulio de Cadilhac <g.decadilhac@gmail.com>
2024-09-26 20:27:56 +0200

    refac: Add functions getPoolValues and isRealPool

--- Challenge2b/src/agent.ts
+++ Challenge2b/src/agent.ts
@@ -8,6 +8,7 @@ import {
   ethers,
 } from "forta-agent";
 import { Provider } from "@ethersproject/providers";
+// import { LRUCache } from "lru-cache";
 import {
   UNI_SWAP_EVENT_ABI,
   UNI_POOL_FUNCTIONS_ABI,
@@ -15,6 +16,51 @@ import {
   UNI_INIT_CODE_HASH,
   CHAIN_IDS,
 } from "./constants";
+
+// const addressIsUniCache = new LRUCache<string, boolean>({ max: 10000 });
+
+const getPoolValues = async (
+  provider: Provider,
+  uniPoolFunctionsAbi: string[],
+  interceptedPoolAddress: string,
+  txEvent: TransactionEvent
+) => {
+  const interceptedPoolContract = new ethers.Contract(
+    interceptedPoolAddress,
+    uniPoolFunctionsAbi,
+    provider
+  );
+  const interceptedPoolValues: any[] = await Promise.all([
+    interceptedPoolContract.token0({ blockTag: txEvent.blockNumber }),
+    interceptedPoolContract.token1({ blockTag: txEvent.blockNumber }),
+    interceptedPoolContract.fee({ blockTag: txEvent.blockNumber }),
+  ]);
+  return interceptedPoolValues;
+};
+
+const isRealPool = async (
+  uniFactoryAddress: string,
+  uniInitCode: string,
+  interceptedPoolAddress: string,
+  interceptedPoolValues: any[]
+) => {
+  const interceptedPoolValuesBytes = ethers.utils.defaultAbiCoder.encode(
+    ["address", "address", "uint24"],
+    interceptedPoolValues
+  );
+  const interceptedSalt = ethers.utils.solidityKeccak256(
+    ["bytes"],
+    [interceptedPoolValuesBytes]
+  );
+  const realPoolAddress = ethers.utils.getCreate2Address(
+    uniFactoryAddress,
+    interceptedSalt,
+    uniInitCode
+  );
+  const isRealPool =
+    realPoolAddress.toLowerCase() === interceptedPoolAddress.toLowerCase();
+  return isRealPool;
+};
 
 export function provideHandleTransaction(
   uniFactoryAddress: string,
@@ -33,46 +79,41 @@ export function provideHandleTransaction(
 
     for (const filteredLog of filteredLogs) {
       const interceptedPoolAddress = filteredLog.address;
-      const interceptedPoolContract = new ethers.Contract(
-        interceptedPoolAddress,
+      // const isPoolInCache = addressIsUniCache.get(interceptedPoolAddress);
+
+      // if (isPoolInCache === false) return findings;
+
+      const interceptedPoolValues = await getPoolValues(
+        provider,
         uniPoolFunctionsAbi,
-        provider
+        interceptedPoolAddress,
+        txEvent
       );
-      const interceptedPoolValues: any[] = await Promise.all([
-        interceptedPoolContract.token0({ blockTag: txEvent.blockNumber }),
-        interceptedPoolContract.token1({ blockTag: txEvent.blockNumber }),
-        interceptedPoolContract.fee({ blockTag: txEvent.blockNumber }),
-      ]);
-      const interceptedPoolValuesBytes = ethers.utils.defaultAbiCoder.encode(
-        ["address", "address", "uint24"],
+
+      const isRealPoolBool: boolean = await isRealPool(
+        uniFactoryAddress,
+        uniInitCode,
+        interceptedPoolAddress,
         interceptedPoolValues
       );
-      const interceptedSalt = ethers.utils.solidityKeccak256(
-        ["bytes"],
-        [interceptedPoolValuesBytes]
-      );
-      const realPoolAddress = ethers.utils.getCreate2Address(
-        uniFactoryAddress,
-        interceptedSalt,
-        uniInitCode
-      );
-      const isRealPool =
-        realPoolAddress.toLowerCase() === interceptedPoolAddress.toLowerCase();
-      if (!isRealPool) return findings;
 
-      const { sender, amount0, amount1 } = filteredLog.args;
+      // addressIsUniCache.set(interceptedPoolAddress, isRealPool);
+
+      if (!isRealPoolBool) return findings;
+
+      const { sender, amount0, amount1, agentId } = filteredLog.args;
 
       findings.push(
         Finding.fromObject({
           name: "Uniswap V3 Swap Detected",
-          description: `Address: ${sender} swapped ${amount0} ${interceptedPoolValues[0]} for ${amount1} ${interceptedPoolValues[1]}, using pool: ${realPoolAddress}`,
+          description: `Address: ${sender} swapped ${amount0} ${interceptedPoolValues[0]} for ${amount1} ${interceptedPoolValues[1]}, using pool: ${interceptedPoolAddress}`,
           alertId: "UNISWAPV3-SWAP-DETECTED",
           severity: FindingSeverity.Info,
           type: FindingType.Info,
           metadata: {
-            agentId: filteredLog.args.agentId?.toString(),
+            agentId: agentId?.toString(),
             chainId,
-            poolAddress: realPoolAddress.toLowerCase(),
+            poolAddress: interceptedPoolAddress.toLowerCase(),
             sender,
             interceptedPoolAddress,
             amount0: amount0.toString(),
